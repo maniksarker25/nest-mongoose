211ba05e102cfe5b9b01b1bcf4df1fc55f814702
manik <maniksarker652@gmail.com>
2025-04-03 14:47:04 +0600

    crearte normal user moduel

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,15 +7,12 @@ import configuration from '../config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      // load: [DatabaseConfig],
       load: configuration,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        // uri: configService.get<string>('database.mongoUri'),
-        // dbName: configService.get<string>('database.dbName'),
         uri: configService.get<string>('database.uri'),
         dbName: configService.get<string>('database.dbName'),
       }),
