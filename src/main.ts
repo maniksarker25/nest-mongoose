16b17152572417fda79b67a81ead007cfdbd4d13
manik <maniksarker652@gmail.com>
2025-04-05 13:00:00 +0600

    make some changes

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './core/app.module';
-// import { Logger, ValidationPipe } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-// import { SendResponseInterceptor } from './common/interceptors/send-response.interceptor';
-// import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   // Enable class-validator globally
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//       transform: true,
-//       stopAtFirstError: true,
-//     }),
-//   );
-//   app.useGlobalInterceptors(new SendResponseInterceptor());
-//   app.useGlobalFilters(new GlobalExceptionFilter());
-//   const configService = app.get(ConfigService);
-//   const port = configService.get<number>('app.port') || 3000;
-
-//   await app.listen(port);
-//   Logger.log(`🚀 App running on http://localhost:${port}`);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -56,6 +28,6 @@ async function bootstrap() {
   const port = configService.get<number>('app.port') || 3000;
 
   await app.listen(port);
-  logger.log(`🚀 App running on http://localhost:${port}`); // ✅ uses Winston-based AppLogger
+  logger.log(`🚀 App running on http://localhost:${port}`);
 }
 bootstrap();
