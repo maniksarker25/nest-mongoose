bd226a800cfba8d169e94d27603715caefb34dc4
manik <maniksarker652@gmail.com>
2025-04-05 17:52:47 +0600

    make some changes

--- src/common/pipes/parse-formdata.pipe.ts
+++ src/common/pipes/parse-formdata.pipe.ts
@@ -1,29 +1,3 @@
-// import {
-//   ArgumentMetadata,
-//   Injectable,
-//   PipeTransform,
-//   BadRequestException,
-// } from '@nestjs/common';
-
-// @Injectable()
-// export class ParseFormDataPipe implements PipeTransform {
-//   transform(value: any, metadata: ArgumentMetadata) {
-//     if (typeof value === 'object' && value.data) {
-//       try {
-//         const parsed = JSON.parse(value.data);
-//         return { ...value, ...parsed }; // Merge parsed data into the body
-//       } catch (err) {
-//         throw new BadRequestException('Invalid JSON in "data" field');
-//       }
-//     }
-//     return value;
-//   }
-// }
-
-// for class supoort
-// src/common/pipes/parse-formdata.pipe.ts
-// src/common/pipes/parse-formdata.pipe.ts
-// src/common/pipes/parse-formdata.pipe.ts
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
