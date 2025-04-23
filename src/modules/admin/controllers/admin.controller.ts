6d21e50d0b98ace1b080670411cae49595dc2942
manik <maniksarker652@gmail.com>
2025-04-23 12:07:19 +0600

    create api for change admin status , active , deactivate

--- src/modules/admin/controllers/admin.controller.ts
+++ src/modules/admin/controllers/admin.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Get,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   Query,
   UseGuards,
@@ -13,12 +15,13 @@ import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { USER_ROLE } from 'src/modules/user/interfaces/user-role.type';
+import { User } from 'src/modules/user/schemas/user.schema';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  // create admin --------------
+  // create admin ====================>
   @Post('create-admin')
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(USER_ROLE.superAdmin)
@@ -31,13 +34,31 @@ export class AdminController {
     };
   }
 
-  // get all admin -------
+  // get all admin ==========================>
   @Get('get-all-admin')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(USER_ROLE.superAdmin)
   async getAllAdmin(@Query() query: Record<string, unknown>) {
     const result = await this.adminService.getAllAdminFromDB(query);
     return {
       statusCode: HttpStatus.OK,
       message: 'All admin retrieved successfully',
+      data: result,
+    };
+  }
+
+  // change admin status ===================================>
+  @Patch('/change-status/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(USER_ROLE.superAdmin)
+  async changeAdminStatus(@Param() id: string) {
+    const result = await this.adminService.changeAdminStatusIntoDB(id);
+    const message = (result?.user as User).isActive
+      ? 'Admin activated successfully'
+      : 'Admin deactivated successfully';
+    return {
+      statusCode: HttpStatus.OK,
+      message,
       data: result,
     };
   }
